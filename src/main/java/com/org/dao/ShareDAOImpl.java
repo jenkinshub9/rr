370529eed699f3b7c03c28ee717f0f318f828e5b
mahendra <mahendra.pachpor@birlasoft.com>
2020-02-03 12:58:37 +0530

    share dao services modified

--- src/main/java/com/org/dao/ShareDAOImpl.java
+++ src/main/java/com/org/dao/ShareDAOImpl.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.org.model.ShareVO;
 
 public class ShareDAOImpl implements ShareDAO {
-
+// modified form git
 	public List<ShareVO> getAllPlayers() {
 
 		List<ShareVO>  listOfShare= new ArrayList<ShareVO>();
