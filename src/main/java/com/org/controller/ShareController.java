505c4915e3df2071f4d4c420bb88ca35f114c948
mahendra <mahendra.pachpor@birlasoft.com>
2020-02-12 10:01:12 +0530

    modifing controller first commit

--- src/main/java/com/org/controller/ShareController.java
+++ src/main/java/com/org/controller/ShareController.java
@@ -17,6 +17,7 @@ import com.org.service.ShareServiceImpl;
  
 /**
 * Servlet implementation class LoginController
+//comment updated the controller 
 */
 public class ShareController extends HttpServlet {
     private static final long serialVersionUID = 1L;
