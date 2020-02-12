af0669448d27602fe561dd4a46c1d5f5858dbf08
mahendra <mahendra.pachpor@birlasoft.com>
2020-02-12 10:53:10 +0530

    Revert "modifing controller modified first commit"
    This reverts commit 505c4915e3df2071f4d4c420bb88ca35f114c948.

--- src/main/java/com/org/controller/ShareController.java
+++ src/main/java/com/org/controller/ShareController.java
@@ -17,7 +17,6 @@ import com.org.service.ShareServiceImpl;
  
 /**
 * Servlet implementation class LoginController
-//comment updated the controller 
 */
 public class ShareController extends HttpServlet {
     private static final long serialVersionUID = 1L;
